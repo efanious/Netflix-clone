b5cf7bf43c649dd798913f0ea95cfc7fd81027f7
Efana <efana.usua@wemabank.com>
2021-07-13 00:20:50 +0100

    connecting end points

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix project</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
     </div>
   );
 }
