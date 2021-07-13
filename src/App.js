e9244a94981a3d146862eac42c806c7d0ed08321
Efana <efana.usua@wemabank.com>
2021-07-13 13:16:26 +0100

    Modified movies row

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
+
   return (
+
     <div className="App">
+
       <h1>Netflix project</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+
     </div>
+
   );
 }
 
--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
 
-function Row({title}) {
-    return (
-        <div>
-            <h2>{title}</h2>
-            {/* title */}
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-            {/* container -> posters */}
-            
-        </div>
-    )
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  // Specific piece of code which runs based on certain properties
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+
+      setMovies(request.data.results);
+
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {/* several row_poster(s)  */}
+
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
